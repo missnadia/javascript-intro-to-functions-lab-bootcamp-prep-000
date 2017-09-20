38dfbd0caa40670de220599a2ce734ccae81abf5
Nadia Lee <miss.nadia.lee@gmail.com>
2017-09-20 06:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function logWhisper(string) {
       console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  console.log(string.toLowerCase())
+  string === string.toLowerCase()
   return("I can\'t hear you!")
 }
 function sayHiToGrandma(string) {
-  console.log(string === string.toUpperCase())
+  string === string.toUpperCase())
   return("YES INDEED!")
 }
 function sayHiToGrandma(string) {
